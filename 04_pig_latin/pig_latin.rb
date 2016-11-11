be4bf1095e45ef2eb99e71abaa7229a078560842
Thach <nonedev@outlook.com>
2016-11-12 06:58:16 +0700

    added pig latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,22 @@
 #write your code here
+def translate(words)
+	vowel = ['a', 'e', 'u', 'i', 'o']
+	two_consonants = ['ch', 'qu', 'th', 'br']
+	three_consonants = ['thr', 'sch', 'squ']
+
+	words.split.map do |word|
+
+		if three_consonants.include? word[0..2]
+			word << word.slice!(0..2) + "ay"
+		elsif two_consonants.include? word[0..1]
+			word << word.slice!(0..1) + "ay"
+		elsif vowel.include? word[0]
+			word << "ay"
+		else
+			word << word.slice!(0) + "ay"
+		end
+		
+		word
+
+	end.join(" ")
+end
