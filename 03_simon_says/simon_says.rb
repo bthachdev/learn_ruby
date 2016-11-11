01ec4618bad8cde659ffe19653f69e6af3a7e949
Thach <nonedev@outlook.com>
2016-11-12 06:15:56 +0700

    added function to convert words simon says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,28 @@
 #write your code here
+def echo(word)
+	word
+end
+
+def shout(word)
+	word.upcase
+end
+
+def repeat(word, times=2)
+	value = "#{word} " * times
+	value.strip
+end
+
+def start_of_word(word, index)
+	word[0...index]
+end
+
+def first_word(word)
+	word.partition(" ").first
+	# word.split(" ").first
+end
+
+def titleize(words)
+	words = words.capitalize
+	excludes = ["and", "over", "the"]
+	words.split.map { |word| (excludes.include? word) ? word: word.capitalize }.join(" ")
+end
