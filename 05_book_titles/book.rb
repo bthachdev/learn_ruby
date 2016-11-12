4609f8b87cc7cefdc60b0946a446182e4cf1288a
Thach <nonedev@outlook.com>
2016-11-12 07:32:27 +0700

    added class Book

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,24 @@
 class Book
 # write your code here
+	attr_writer :title
+
+	def initialize
+		@conjunctions = ['and', 'or']	
+		@prepositions = ['in', 'the', 'of']
+		@articles = ['a', 'an']
+	end
+
+	def title
+		new_title = @title.capitalize
+		new_title = new_title.split.map do |word| 
+
+			if @conjunctions.include? word or @prepositions.include? word or @articles.include? word
+				word
+			else
+				word.capitalize	
+			end
+		end.join(" ")
+
+		new_title
+	end
 end
